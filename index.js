210c946019f3bfda05881438f626f900c55f36e7
raphaeldieu <dieuraphael@gmail.com>
2015-07-31 11:36:24 +0800

    typo...

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = new Hapi.Server();
 //Configure server connections / host
 server.connection({
   host:'0.0.0.0',
-  port: process.enc.PORT || 3000,
+  port: process.env.PORT || 3000,
   routes: {
     cors:{
       headers: ["Access-Control-Allow-Credentials"],
